797b1631661cb766fff9203b0caac1ed0a752703
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-25 14:36:18 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+terraform {
+    backend "gcs" { 
+      bucket  = " learn-github-action-terraform-and-gcp"
+      prefix  = "demo"
+    }
+}
 
+provider "google" {
+  project = var.project
+  region = var.region
+}
