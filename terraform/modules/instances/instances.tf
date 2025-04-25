921938f5ce9cd518f3b8800f613e3df44c6ffa2d
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-25 16:24:27 -0500

    Update instances.tf

--- terraform/modules/instances/instances.tf
+++ terraform/modules/instances/instances.tf
@@ -1,1 +1,16 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance-00"
+  machine_type = "f1-micro"
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  #network_interface {
+  #  network = google_compute_network.vpc_network.name
+  #  access_config {
+  #  }
+  #}
+}
