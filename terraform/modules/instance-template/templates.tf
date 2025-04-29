cd06023a21800e329773c345454e81e8d1b039a9
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-29 13:59:59 -0500

    Create templates.tf

---
+++ terraform/modules/instance-template/templates.tf
@@ -0,0 +1,31 @@
+resource "google_compute_instance_template" "vm_instance_template" {
+  name         = "terraform-instance-template-01"
+  machine_type = "f1-micro"
+
+  disk {
+    auto_delete  = true
+    boot         = true
+    source_image = "debian-cloud/debian-11" 
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral external IP address
+    }
+  }
+
+  metadata = {
+    startup-script = <<-EOT
+      #!/bin/bash
+      sudo apt-get update
+      sudo apt-get install -y apache2
+      echo "Hello world from $(hostname) $(hostname -i)" > /var/www/html/index.html
+      sudo systemctl start apache2
+      sudo systemctl enable apache2
+    EOT
+  }
+
+  tags = ["http-server"]
+}
