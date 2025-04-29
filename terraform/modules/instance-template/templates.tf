27a16d9d65523a223bf263e2b9eb82bdfb419af4
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-29 16:36:54 -0500

    Update templates.tf

--- terraform/modules/instance-template/templates.tf
+++ terraform/modules/instance-template/templates.tf
@@ -5,7 +5,8 @@ resource "google_compute_instance_template" "vm_instance_template" {
   disk {
     auto_delete  = true
     boot         = true
-    source_image = "debian-cloud/debian-11" 
+    #source_image = "debian-cloud/debian-11"
+    source_image - projects/optical-figure-453316-a4/global/images/my-image-1
   }
 
   network_interface {
@@ -19,8 +20,8 @@ resource "google_compute_instance_template" "vm_instance_template" {
   metadata = {
     startup-script = <<-EOT
       #!/bin/bash
-      sudo apt-get update
-      sudo apt-get install -y apache2
+        #sudo apt-get update
+        #sudo apt-get install -y apache2
       echo "Hello world from $(hostname) $(hostname -i)" > /var/www/html/index.html
       sudo systemctl start apache2
       sudo systemctl enable apache2
