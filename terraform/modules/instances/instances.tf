2f91c1cda81b50a5ee840e5784ce12240486bd99
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-29 14:00:44 -0500

    Update instances.tf

--- terraform/modules/instances/instances.tf
+++ terraform/modules/instances/instances.tf
@@ -27,16 +27,3 @@ metadata = {
 
 tags = ["http-server"]
 }
-#--------------------------------------------#
-# Add a firewall rule to allow HTTP traffic
-resource "google_compute_firewall" "default-allow-http" {
-  name    = "default-allow-http"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["http-server"]
-}
