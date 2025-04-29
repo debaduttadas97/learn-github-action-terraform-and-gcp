a9451235835ae96ebe83d70ab57e600f353166f6
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-29 15:21:19 -0500

    Update variables.tf

--- terraform/modules/managed-instance-group/variables.tf
+++ terraform/modules/managed-instance-group/variables.tf
@@ -3,3 +3,7 @@ variable "mig-zone" {
  default= "us-central1-a"
  description = "Default ZONE for Managed Instance Group"
 }
+variable "instance_template_self_link" {
+  description = "The self_link of the instance template to use for the managed instance group"
+  type        = string
+}
