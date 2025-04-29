57cf3998ad764eb7488f5f718a10646f2ff0170e
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-29 15:58:10 -0500

    Update outputs.tf

--- terraform/modules/instance-template/outputs.tf
+++ terraform/modules/instance-template/outputs.tf
@@ -1,3 +1,3 @@
 output "template_selflink_unique" {
-  value = google_compute_instance_template.vm_instance_template.selflink
+  value = google_compute_instance_template.vm_instance_template.self_link
 }
