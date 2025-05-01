2a58d8f47018ce842fb22124d04dfe234281da57
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-30 21:36:58 -0500

    Update outputs.tf

--- terraform/modules/managed-instance-group/outputs.tf
+++ terraform/modules/managed-instance-group/outputs.tf
@@ -1,3 +1,7 @@
 output "health_check_selflink_unique" {
   value = google_compute_health_check.default.self_link
 }
+
+output "instance_group_self_link" {
+  value = google_compute_region_instance_group_manager.default.instance_group
+}
