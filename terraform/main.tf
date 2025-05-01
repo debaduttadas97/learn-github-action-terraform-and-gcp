b7a496025809ab3c9c0ed148faef0274d8782a7e
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-30 22:17:55 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,25 +11,25 @@ provider "google" {
   zone = var.zone
 }
 
-module "instances" {
-  source     = "./modules/instances"
-}
+#module "instances" {
+#  source     = "./modules/instances"
+#}
 
-module "firewalls" {
-    source    = "./modules/firewalls"
-}
+#module "firewalls" {
+#    source    = "./modules/firewalls"
+#}
 
-module "instance-template" {
-    source    = "./modules/instance-template"
-}
+#module "instance-template" {
+#    source    = "./modules/instance-template"
+#}
 
-module "managed-instance-group" {
-    source    = "./modules/managed-instance-group"
-    instance_template_self_link    = module.instance-template.template_selflink_unique
-}
+#module "managed-instance-group" {
+#    source    = "./modules/managed-instance-group"
+#    instance_template_self_link    = module.instance-template.template_selflink_unique
+#}
 
-module "http-load-balancer" {
-    source    = "./modules/http-load-balancer"
-    health_check_self_link    = module.managed-instance-group.health_check_selflink_unique
-    instance_group    = module.managed-instance-group.instance_group_self_link
-}
+#module "http-load-balancer" {
+#    source    = "./modules/http-load-balancer"
+#    health_check_self_link    = module.managed-instance-group.health_check_selflink_unique
+#    instance_group    = module.managed-instance-group.instance_group_self_link
+#}
