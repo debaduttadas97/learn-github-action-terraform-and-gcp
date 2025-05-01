d07baf5432f12fae3095d8b8fe3a83f180699b6f
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-30 21:50:59 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,5 +31,5 @@ module "managed-instance-group" {
 module "http-load-balancer" {
     source    = "./modules/http-load-balancer"
     health_check_self_link    = module.managed-instance-group.health_check_selflink_unique
-    instance_group    = module.managed_instance_group.instance_group_self_link
+    instance_group    = module.managed-instance-group.instance_group_self_link
 }
