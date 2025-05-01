fecebe99ada06e3ec63ed61602b2f0b5ec105261
Debadutta Das <85604680+debaduttadas97@users.noreply.github.com>
2025-04-30 21:38:27 -0500

    Update variables.tf

--- terraform/modules/http-load-balancer/variables.tf
+++ terraform/modules/http-load-balancer/variables.tf
@@ -2,3 +2,8 @@ variable  "health_check_self_link" {
     type = string
     description = "Managed Instance group health_check self_link"
 }
+
+variable "instance_group" {
+  description = "The self_link of the instance group"
+  type        = string
+}
